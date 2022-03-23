27ff17c2a7f62cab7199d8ac22c9b6113ca7a886
OrigamiPaper225 <lohjames16@gmail.com>
2022-03-23 14:06:21 -0400

    fixed debug

--- assets/js/startpage.js
+++ assets/js/startpage.js
@@ -1,29 +1,9 @@
 function fadeout() {
     const myBody = document.body.innerHTML;
     document.body.style.opacity=0;
-
-    // document.getElementByClass("container").style.fade = "0";
-    
-    changePage();
+    console.log(document.body.style.opacity);
+    function changePage(){
+        setTimeout(function() {window.location.href = "index.html"}, 1000);
+     }
+     changePage();
 }
-
-function changePage() {
-    setTimeout(window.location.href = "index.html", 5000);
-}
-
-
-
-// document.body.classList.add('fade');
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     window.setTimeout(function() {
-//       document.body.classList.remove('fade');
-//     }, 5000);
-//     window.location.href = "index.html";
-//   });
-
-
-
-// function windowreplace() {
-    
-// }
